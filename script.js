a90fd1c4cd2999998de2c2123ba30ee7eaf9fea7
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-21 01:38:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const quizdata = [
     {
-        question: 'Which language runs in a web browser?'
+        question: 'Which language runs in a web browser?',
+        a: "Java",
+        b: "C",
+        c: "JavaScript",
+        d: "Python",
+        correct: "c",
+    },
+    {
+        question: "What does CSS stands For??",
+        a: "Central Style Sheet",
+        b: "Cascading Style Sheet",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stands For???",
+        a: "HyperText Markup Language",
+        b: "HyperText Markdown Language",
+        c: "Hyperloop Machine language",
+        d: "Helicopters Terminals Motorboats Lamborghinis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript Launched?",
+        a: "1996",
+        b:"1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
     }
-]
+];
